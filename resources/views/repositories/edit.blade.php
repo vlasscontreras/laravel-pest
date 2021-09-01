169d3abd864cc65ce16282792a37525e221e328a
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-09-01 17:48:21 -0600

    💄 Update the styles of the create/edit repository screens

--- resources/views/repositories/edit.blade.php
+++ resources/views/repositories/edit.blade.php
@@ -7,21 +7,43 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <form action="{{ route('repositories.update', $repository) }}" method="POST">
-                    @method('PATCH')
-                    @csrf
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1 flex justify-between">
+                    <div class="px-4 sm:px-0">
+                        <h3 class="text-lg font-medium text-gray-900">{{ __('Repository Information') }}</h3>
 
-                    <label for="url">{{ __('URL') }}</label>
-                    <input type="text" name="url" id="url" class="form-input mt-1 block w-full" value="{{ old('url', $repository->url) }}">
+                        <p class="mt-1 text-sm text-gray-600">
+                            {{ __('Add the repository URL (GitHub, GitLab, BitBucket, etc.) and a short description.') }}
+                        </p>
+                    </div>
+                </div>
 
-                    <label for="description">{{ __('Description') }}</label>
-                    <textarea name="description" id="description" class="form-textarea mt-1 block w-full" rows="3">
-                        {{ old('description', $repository->description) }}
-                    </textarea>
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    <form action="{{ route('repositories.update', $repository) }}" method="POST">
+                        @method('PATCH')
+                        @csrf
+                        <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+                            <div class="grid grid-cols-6 gap-6">
+                                <div class="col-span-6 sm:col-span-4">
+                                    <x-jet-label for="url" value="{{ __('URL') }}" />
+                                    <x-jet-input id="url" type="url" name="url" placeholder="https://github.com/vlasscontreras/laravel-pest" class="mt-1 block w-full" value="{{ old('url', $repository->url) }}" />
+                                    <x-jet-input-error for="url" class="mt-2" />
+                                </div>
+                                <div class="col-span-6 sm:col-span-4">
+                                    <x-jet-label for="description" value="{{ __('Description') }}" />
+                                    <x-jet-input id="description" type="text" name="description" placeholder="{{ __('A Laravel application that uses Pest as testing library.') }}" value="{{ old('description', $repository->description) }}" class="mt-1 block w-full" />
+                                    <x-jet-input-error for="description" class="mt-2" />
+                                </div>
+                            </div>
+                        </div>
 
-                    <button type="submit" class="btn btn-primary mt-4">{{ __('Update') }}</button>
-                </form>
+                        <div class="flex items-center justify-start px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                            <x-jet-button type="submit">
+                                {{ __('Save Repository') }}
+                            </x-jet-button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
