a82ac313106b1d01d0cfcecdfd168f0f1b6b008a
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-09-01 17:06:46 -0600

    💄 Improve styles of the private repository index

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,39 +7,53 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="px-4 py-2">{{ __('ID') }}</th>
-                            <th class="px-4 py-2">{{ __('URL') }}</th>
-                            <th class="px-4 py-2">{{ __('Description') }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($repositories as $repository)
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="shadow overflow-auto border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
                             <tr>
-                                <td class="border px-4 py-2">{{ $repository->id }}</td>
-                                <td class="border px-4 py-2">
-                                    <a href="{{ route('repositories.show', $repository) }}">
-                                        {{ $repository->url }}
-                                    </a>
-                                    <a href="{{ route('repositories.edit', $repository) }}">
-                                        {{ __('Edit') }}
-                                    </a>
-                                </td>
-                                <td class="border px-4 py-2">{{ $repository->description }}</td>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('ID') }}</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('URL') }}</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Description') }}</th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
                             </tr>
-                        @empty
-                            <tr>
-                                <td class="border px-4 py-2" colspan="3">
-                                    {{ __('No repositories found.') }}
-                                </td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+
+                        <tbody>
+                            @forelse ($repositories as $repository)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $repository->id }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <a href="{{ $repository->url }}" target="_blank">
+                                            {{ $repository->url }}
+                                        </a>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $repository->description }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <a href="{{ route('repositories.edit', $repository) }}">
+                                            {{ __('Edit') }}
+                                        </a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" colspan="3">
+                                        {{ __('No repositories found.') }}
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
+
+            <a href="{{ route('repositories.create') }}" class="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 fixed bottom-5 right-5">
+                <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                </svg>
+            </a>
         </div>
     </div>
 </x-app-layout>
